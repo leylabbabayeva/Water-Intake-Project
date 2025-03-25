39353b595537e96d4234f7688b9b2c6e71334b54
leylabbabayeva <lejlababayeva25@gmail.com>
2025-03-25 18:58:15 +0100

    Commit Initial File

--- water-intake.js
+++ water-intake.js
@@ -9,11 +9,11 @@ function increment() {
     let image = document.getElementById("water-img");
 
     if (count < 4) {
-        image.src = "low_water.png";  // Image for less than 4 glasses
+        image.src = "img/low_water.png";  // Image for less than 4 glasses
     } else if (count >= 4 && count <= 6) {
-        image.src = "middle_water.png";  // Image for 4-6 glasses
+        image.src = "img/middle_water.png";  // Image for 4-6 glasses
     } else {
-        image.src = "high_water.png";  // Image for more than 7 glasses
+        image.src = "img/high_water.png";  // Image for more than 7 glasses
     }
 }
 
@@ -23,5 +23,5 @@ function save() {
     countEl.textContent = 0
     count = 0
     let image = document.getElementById("water-img");
-    image.src = "low_water.png";
+    image.src = "img/low_water.png";
 }
